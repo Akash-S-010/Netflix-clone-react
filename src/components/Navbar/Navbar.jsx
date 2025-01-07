473cc334d57bc149029dc04b67ed6d11858a3a92
Akash S <akashspalloor@gmail.com>
2025-01-07 20:53:27 +0530

    Navbar styled and mounted in home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search_icon.svg";
+import bell_icon from "../../assets/bell_icon.svg";
+import profile from "../../assets/profile_img.png";
+import caret from "../../assets/caret_icon.svg";
 
 function Navbar() {
   return (
-    <div>
-      
+    <div className="navbar">
+      <div className="logo-section">
+        <img src={logo} alt="logo" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by languages</li>
+        </ul>
+      </div>
+      <div className="right-section">
+        <img src={search_icon} alt="search" className="icons" />
+        <p>Children</p>
+        <img src={bell_icon} alt="bell" className="icons"/>
+        <div className="nav-profile">
+          <img src={profile} alt="profile" className="profile" />
+          <img src={caret} alt="caret" />
+          <div className="dropdown">
+            <p>Sign Out of Netflix</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
