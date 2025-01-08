e726a5c63dc291e807e2f9e3bb327c48008dee06
Akash S <akashspalloor@gmail.com>
2025-01-08 17:18:30 +0530

    created login form and set the route for the /login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from "react"
-import Home from "./pages/Homepage/Home"
+import React from "react";
+import Home from "./pages/Homepage/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
 function App() {
-
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
