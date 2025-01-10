de688bd58c16dbcbce04f340896aee4e5eb26a89
Akash S <akashspalloor@gmail.com>
2025-01-10 16:29:35 +0530

    connected the firebase auth to login page with functionality

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
 import { useState } from "react";
+import { logIn, signUp } from "../../firebase";
 
 function Login() {
   const [signState, setSignState] = useState("Sign In");
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const user_auth = (event) => {
+    event.preventDefault();
+    if (signState === "Sign Up") {
+      signUp(name, email, password);
+    } else {
+      logIn(email, password);
+    }
+  }
 
   return (
     <div className="login">
@@ -13,14 +27,14 @@ function Login() {
         <h1>{signState}</h1>
         <form>
           {signState === "Sign Up" ? (
-            <input type="text" placeholder="Your Name" />
+            <input type="text" placeholder="Your Name" value={name} onChange={(e)=>setName(e.target.value)} />
           ) : (
             <></>
           )}
 
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">{signState}</button>
+          <input type="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+          <button type="submit" onClick={user_auth}>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
